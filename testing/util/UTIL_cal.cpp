d0ff2eebb1b11f53704e32c3dde0e8bab2183ce4
Matthias Bach <bach@compeng.uni-frankfurt.de>
2010-08-26 21:25:17 +0200

    Adjusted to new caldgemm API

--- testing/util/UTIL_cal.cpp
+++ testing/util/UTIL_cal.cpp
@@ -15,21 +15,23 @@ void CALDGEMM_Init()
 {
 	cal_info.Pin = -3;
 	cal_info.Verify = CAL_FALSE;
-	cal_info.PrintIL = CAL_FALSE;
 	cal_info.Disassemble = CAL_FALSE;
 	cal_info.Quiet = CAL_FALSE;
 	cal_info.DeviceNum = 0;
 	cal_info.Width = 1024; //k for matrix multiply
 	cal_info.Height = 2048;
+	cal_info.AutoHeight = CAL_TRUE;
 	cal_info.Iterations = 1;
 	cal_info.DstMemory = 'c';
-	cal_info.DstCacheable = CAL_TRUE;
 	cal_info.VerboseTiming = CAL_FALSE;
 	cal_info.Debug = CAL_FALSE;
 	cal_info.MultiThread = CAL_TRUE;
 	cal_info.UseGPU = CAL_TRUE;
 	cal_info.UseCPU = CAL_TRUE;
-	cal_info.GPURatio = 0.66;
+	cal_info.GPURatio = -1;
+	cal_info.DynamicSched = CAL_TRUE;
+	cal_info.MemPolicy = CAL_TRUE;
+	cal_info.DumpMatrix = CAL_FALSE;
 
 	cal_dgemm.InitCALDGEMM( &cal_info );
 }
@@ -45,20 +47,15 @@ void CALDGEMM_dgemm( const enum CBLAS_ORDER ORDER, const enum CBLAS_TRANSPOSE TR
                      const double * B, const int LDB, const double BETA, double * C,
                      const int LDC )
 {
-	// check wether K matches the restrictions of calblas
-	if( K == cal_info.Width && ORDER == CblasColMajor && TRANSA == CblasNoTrans && TRANSB == CblasNoTrans )
-	{
-		// cal_dgemm assumes row major order but linpack is always column major
-		// therefore it needs to work on C^T -> we need to switch A and B
-		// TODO insert a check, though redundent
-		cal_dgemm.ResetTimers();
-		cal_dgemm.RunCALDGEMM( (double*) B, (double*) A, C, (double) ALPHA, (double) BETA, (int) N, (int) M, (int) LDB, (int) LDA, (int) LDC );
-	}
-	else
-	{
-		// Use plain cblas
-		cblas_dgemm( ORDER, TRANSA, TRANSB, M, N, K, ALPHA, (double*) A, LDA, (double*) B, LDB,
-		             BETA, C, LDC );
-	}
+        if( M >= 2048 && N >= 2048 && K >= 512 )
+        {
+	    cal_dgemm.RunCALDGEMM( (double*) A, (double*) B, C, (double) ALPHA, (double) BETA, (int) M, (int) K, (int) N, (int) LDA, (int) LDB, (int) LDC, ORDER, TRANSA, TRANSB );
+        }
+        else
+        {
+            // Use plain cblas
+            cblas_dgemm( ORDER, TRANSA, TRANSB, M, N, K, ALPHA, (double*) A, LDA, (double*) B, LDB, BETA, C, LDC );
+        }
+
 }
 
