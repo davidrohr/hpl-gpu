2169000b42e96f7cce83ec08c5c8a7a6ee048acf
Matthias Kretz <kretz@kde.org>
2010-09-27 10:38:33 +0200

    fix copyright information and simplify fast exit

--- src/pauxil/HPL_dlaswp10N.cpp
+++ src/pauxil/HPL_dlaswp10N.cpp
@@ -1,64 +1,15 @@
-/* 
- * This is a modified version of the High Performance Computing Linpack
- * Benchmark (HPL). All code not contained in the original HPL version
- * 2.0 is property of the Frankfurt Institute for Advanced Studies
- * (FIAS). None of the material may be copied, reproduced, distributed,
- * republished, downloaded, displayed, posted or transmitted in any form
- * or by any means, including, but not limited to, electronic,
- * mechanical, photocopying, recording, or otherwise, without the prior
- * written permission of FIAS. For those parts contained in the
- * unmodified version of the HPL the below copyright notice applies.
- * 
- * Authors:
- * David Rohr (drohr@jwdt.org)
- * Matthias Bach (bach@compeng.uni-frankfurt.de)
- * Matthias Kretz (kretz@compeng.uni-frankfurt.de)
- * 
- * -- High Performance Computing Linpack Benchmark (HPL)                
- *    HPL - 2.0 - September 10, 2008                          
- *    Antoine P. Petitet                                                
- *    University of Tennessee, Knoxville                                
- *    Innovative Computing Laboratory                                 
- *    (C) Copyright 2000-2008 All Rights Reserved                       
- *                                                                      
- * -- Copyright notice and Licensing terms:                             
- *                                                                      
- * Redistribution  and  use in  source and binary forms, with or without
- * modification, are  permitted provided  that the following  conditions
- * are met:                                                             
- *                                                                      
- * 1. Redistributions  of  source  code  must retain the above copyright
- * notice, this list of conditions and the following disclaimer.        
- *                                                                      
- * 2. Redistributions in binary form must reproduce  the above copyright
- * notice, this list of conditions,  and the following disclaimer in the
- * documentation and/or other materials provided with the distribution. 
- *                                                                      
- * 3. All  advertising  materials  mentioning  features  or  use of this
- * software must display the following acknowledgement:                 
- * This  product  includes  software  developed  at  the  University  of
- * Tennessee, Knoxville, Innovative Computing Laboratory.             
- *                                                                      
- * 4. The name of the  University,  the name of the  Laboratory,  or the
- * names  of  its  contributors  may  not  be used to endorse or promote
- * products  derived   from   this  software  without  specific  written
- * permission.                                                          
- *                                                                      
- * -- Disclaimer:                                                       
- *                                                                      
- * THIS  SOFTWARE  IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,  INCLUDING,  BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE UNIVERSITY
- * OR  CONTRIBUTORS  BE  LIABLE FOR ANY  DIRECT,  INDIRECT,  INCIDENTAL,
- * SPECIAL,  EXEMPLARY,  OR  CONSEQUENTIAL DAMAGES  (INCLUDING,  BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA OR PROFITS; OR BUSINESS INTERRUPTION)  HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT,  STRICT LIABILITY,  OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
- * ---------------------------------------------------------------------
- */ 
+/*
+    Copyright (C) 2010 Matthias Kretz <kretz@kde.org>
+    Copyright (C) 2010 Frankfurt Institute for Advanced Studies (FIAS)
+
+    The source code is property of the Frankfurt Institute for Advanced Studies
+    (FIAS). None of the material may be copied, reproduced, distributed,
+    republished, downloaded, displayed, posted or transmitted in any form or by
+    any means, including, but not limited to, electronic, mechanical,
+    photocopying, recording, or otherwise, without the prior written permission
+    of FIAS.
+
+*/
 
 #include "util_timer.h"
 #include "util_trace.h"
@@ -73,7 +24,9 @@ extern "C" void HPL_dlaswp10N(const int M, const int N, double *A,
    int realN = 0;
 #endif /* TRACE_CALLS */
 
-   if( ( M <= 0 ) || ( N <= 0 ) ) return;
+   if (M <= 0) {
+       return;
+   }
 
    const int mu = M & ~15u;
    const int mr = M - mu;
