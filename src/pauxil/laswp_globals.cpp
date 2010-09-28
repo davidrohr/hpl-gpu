7f9e245f9f9e961c845666228e4a1ff047eb1a95
Matthias Kretz <kretz@kde.org>
2010-09-28 17:11:05 +0200

    debug output for TBB initialization; init with automatic

--- src/pauxil/laswp_globals.cpp
+++ src/pauxil/laswp_globals.cpp
@@ -13,15 +13,19 @@
 
 #include <tbb/task_scheduler_init.h>
 #include <cstdlib>
+#include <cstdio>
 
 namespace
 {
     int HPL_init_laswp()
     {
         const char *num_threads_string = getenv("LASWP_NUM_THREADS");
-        int num_threads = tbb::task_scheduler_init::default_num_threads();
+        int num_threads = tbb::task_scheduler_init::automatic;
         if (num_threads_string) {
             num_threads = atoi(num_threads_string);
+            fprintf(stderr, "TBB initialized with %d threads\n", num_threads);
+        } else {
+            fprintf(stderr, "TBB initialized: %d threads available\n", tbb::task_scheduler_init::default_num_threads());
         }
         static tbb::task_scheduler_init init(num_threads);
         return 0;
