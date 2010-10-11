a2d633289031e12a78b82a6b6214786b980f2ecf
David Rohr <drohr@jwdt.org>
2010-10-11 13:09:22 +0200

    remove HPL_rollN

--- include/hpl_pgesv.h
+++ include/hpl_pgesv.h
@@ -258,18 +258,6 @@ STDC_ARGS( (
    const int *,
    int *
 ) );
-void                             HPL_rollN
-STDC_ARGS( (
-   HPL_T_panel *,
-   int *,
-   HPL_T_panel *,
-   const int,
-   double *,
-   const int,
-   const int *,
-   const int *,
-   const int *
-) );
 void                             HPL_rollT
 STDC_ARGS( (
    HPL_T_panel *,
