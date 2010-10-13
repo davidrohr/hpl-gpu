cb512dd8e14dd3ae464c729d3c1054c3fd1281fd
David Rohr <drohr@jwdt.org>
2010-10-14 00:36:36 +0200

    make all debug output go to stderr

--- testing/ptimer/HPL_ptimer.c
+++ testing/ptimer/HPL_ptimer.c
@@ -170,7 +170,7 @@ void HPL_ptimer( const int I )
         case 21: sprintf(timer_name, "BCAST UPD");break;
         default: sprintf(timer_name, "Unknown");break;
       }
-      if (I != 13 && I != 12) printf("Timer %s (%d) CPU Time %2.5lf Wall Time %2.5lf\n", timer_name, I, cpu_tmp_time, wall_tmp_time);
+      if (I != 13 && I != 12) fprintf(stderr, "Timer %s (%d) CPU Time %2.5lf Wall Time %2.5lf\n", timer_name, I, cpu_tmp_time, wall_tmp_time);
 #endif
    }
 /*
